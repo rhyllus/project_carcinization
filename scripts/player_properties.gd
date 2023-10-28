a991a58876fc5f23507a26cf80bc182ea2b8fb2c
rhyllus <sashatcher2013@yandex.ru>
2023-10-28 23:41:15 +0300

    this isnt working well

--- scripts/player_properties.gd
+++ scripts/player_properties.gd
@@ -4,27 +4,26 @@ class_name PlayerProperties
 
 var gravity : float = ProjectSettings.get_setting("physics/3d/default_gravity")
 
-enum player_states {FLYING, GROUNDED, BREAKS, ACTION}
+enum player_states {FLYING, GROUNDED, BREAKS, ACTION, FLOATING}
 var player_state := player_states.FLYING
 
 var input_dir : Vector3
 
 @export var START_SPEED := 7.0
 @export var JOG_START := 26.5
-var will_hit_breaks : bool = true
 @export var SPEED_CAP := 50.4
 var SPEED = START_SPEED
-@export var LOSS_WALKING := 40.0
-@export var LOSS_BREAKS := 27.6
+@export var LOSS_WALKING := 50
+@export var LOSS_BREAKS := 35
 @export var LOSS_RUNNING := 20.0
 var RUNNING_STATE : bool = false
 var COLLIDED_LAST_FRAME : bool = false
 @export var ACCELERATION := 19.8
 @export var TURNING_SPEED := 4.8
+var is_on_wall_extra : bool = false
 
-@export var JUMP_START_SPEED := 30.0
-@export var JUMP_SPEED_CAP := 40.0
-@export var JUMP_DECELERATION := 380.0
+@export var JUMP_START_SPEED := 500.0
+@export var JUMP_DECELERATION := 2900.0
 var JUMP_VELOCITY = JUMP_START_SPEED
 var APPLIED_JUMP_ACCELERATION : float
 var APPLIED_START_SPEED : float
@@ -41,7 +40,8 @@ var CollisionShape
 var JumpTimer
 var JumpBuffer
 var ZeroGravity
-var VectorTwist
+var VectorTwist_x
+var VectorTwist_z
 var VectorPitch
 var Front
 var Back
@@ -70,10 +70,12 @@ func _init():
 	CharacterBody.add_child(JumpBuffer)
 	CharacterBody.add_child(ZeroGravity)
 	
-	VectorTwist = Node3D.new()
-	CharacterBody.add_child(VectorTwist)
+	VectorTwist_x = Node3D.new()
+	CharacterBody.add_child(VectorTwist_x)
+	VectorTwist_z = Node3D.new()
+	VectorTwist_x.add_child(VectorTwist_z)
 	VectorPitch = Node3D.new()
-	VectorTwist.add_child(VectorPitch)
+	VectorTwist_z.add_child(VectorPitch)
 	Front = RayCast3D.new()
 	Front.target_position.y = -1.15
 	Front.debug_shape_custom_color = Color(1, 1, 0)
@@ -131,31 +133,38 @@ func vector_angle_calculator(vect1 : Vector3, vect2 : Vector3):
 	var angle_x : float
 	var angle_z : float
 	dir_cast.z = 0
-	angle_z = vect1.angle_to(dir_cast)
+	angle_z = vect1.angle_to(dir_cast.normalized())
 	if dir_cast.x > 0:
 		angle_z = -angle_z
 	dir_cast.z = vect2.z
 	dir_cast.x = 0
-	angle_x = vect1.angle_to(dir_cast)
+	angle_x = vect1.angle_to(dir_cast.normalized())
 	if dir_cast.z < 0:
 		angle_x = -angle_x
 	return Vector3(angle_x, 0, angle_z)
 
-func change_gravity(dir : Vector3):
+func change_gravity(dir : Vector3) -> void:
 	var angle_x_z : Vector3 = vector_angle_calculator(Vector3.UP, dir)
 	gravity_last_angles.x = angle_x_z.x
 	gravity_last_angles.z = angle_x_z.z
-	VectorTwist.rotation.z = angle_x_z.z
+	VectorTwist_z.rotation.z = -angle_x_z.z
 	Graphic.rotation.z = angle_x_z.z
-	VectorTwist.rotation.x = angle_x_z.x
+	VectorTwist_x.rotation.x = angle_x_z.x
 	Graphic.rotation.x = angle_x_z.x
 	CharacterBody.up_direction = dir
 
-func movement_reset():
+func movement_reset() -> void:
 	SPEED = START_SPEED
 	chosen_dir = Vector3.ZERO
 
-func horizontal_movement(delta):
+func breaks(delta) -> void:
+	horizontal_velocity = horizontal_velocity.move_toward(Vector3.ZERO, LOSS_BREAKS * delta)
+	CharacterBody.velocity = horizontal_velocity
+	if horizontal_velocity == Vector3.ZERO:
+		player_state = player_states.GROUNDED
+		movement_reset()
+
+func horizontal_movement(delta) -> void:
 	if input_dir:
 		if SPEED < START_SPEED:
 			SPEED = START_SPEED
@@ -165,7 +174,7 @@ func horizontal_movement(delta):
 			if SPEED > SPEED_CAP:
 				SPEED = move_toward(SPEED, SPEED_CAP, ACCELERATION * delta)
 			chosen_dir = chosen_dir.lerp(input_rotated, TURNING_SPEED * delta)
-			if VelocityVector.target_position.angle_to(input_rotated) >= PI/2:
+			if VelocityVector.target_position.angle_to(input_rotated) >= (PI * 0.6):
 				player_state = player_states.BREAKS
 				return
 		else:
@@ -179,10 +188,78 @@ func horizontal_movement(delta):
 				movement_reset()
 	horizontal_velocity = chosen_dir * SPEED
 	if gravity_last_angles.x > 0:
-		horizontal_velocity = horizontal_velocity.rotated(Vector3.FORWARD, gravity_last_angles.z)
-	else:
 		horizontal_velocity = horizontal_velocity.rotated(Vector3.FORWARD, -gravity_last_angles.z)
+	else:
+		horizontal_velocity = horizontal_velocity.rotated(Vector3.FORWARD, gravity_last_angles.z)
 	if gravity_last_angles.z < 0:
 		horizontal_velocity = horizontal_velocity.rotated(Vector3.LEFT, gravity_last_angles.x)
 	else:
 		horizontal_velocity = horizontal_velocity.rotated(Vector3.LEFT, -gravity_last_angles.x)
+
+func compare_center_to_ray(ray : RayCast3D):
+	if ray.is_colliding():
+		for i in CharacterBody.get_slide_collision_count():
+			var slide_collision_point = CharacterBody.get_slide_collision(i).get_position()
+			if slide_collision_point.distance_to(Center.get_collision_point()) > slide_collision_point.distance_to(ray.get_collision_point()):
+				return ray.get_collision_normal()
+	return Vector3.ZERO
+	
+func rotate_player_body(delta, mode : int, lerp_bool : bool) -> void:
+	var collision_normal : Vector3
+	if mode == 1:
+		collision_normal = Vector3.ZERO
+		var last_rotation : Vector3 = Center.get_collision_normal()
+		collision_normal = compare_center_to_ray(Front)
+		if collision_normal == Vector3.ZERO:
+			collision_normal = compare_center_to_ray(Left)
+			if collision_normal == Vector3.ZERO:
+				collision_normal = compare_center_to_ray(Right)
+		if Center.is_colliding():
+			if collision_normal == Vector3.ZERO:
+				collision_normal = last_rotation
+		else:
+			player_state = player_states.FLOATING
+			CharacterBody.velocity = CharacterBody.get_real_velocity()
+			movement_reset()
+			return
+	elif mode == 0:
+		collision_normal = CharacterBody.get_floor_normal()
+	var angle_x_z : Vector3 = vector_angle_calculator(Vector3.UP, collision_normal)
+	if lerp_bool:
+		Graphic.rotation.x = lerp_angle(Graphic.rotation.x, angle_x_z.x, 15 * delta)
+		Graphic.rotation.z = lerp_angle(Graphic.rotation.z, angle_x_z.z, 15 * delta)
+	VectorTwist_x.rotation.x = angle_x_z.x
+	VectorTwist_z.rotation.z = angle_x_z.z
+
+func jump_input(delta) -> void:
+	if Input.is_action_just_pressed("jump"):
+		if not CharacterBody.is_on_floor():
+			JumpBuffer.start(0.22)
+		elif CharacterBody.is_on_floor():
+			CharacterBody.velocity.y = CharacterBody.get_real_velocity().y
+			CharacterBody.velocity.y += JUMP_VELOCITY * delta
+			APPLIED_START_SPEED = CharacterBody.velocity.y
+			JumpTimer.start(0.2)
+	elif Input.is_action_pressed("jump") and not JumpTimer.is_stopped():
+		JUMP_VELOCITY = move_toward(JUMP_VELOCITY, 0, JUMP_DECELERATION * delta)
+		CharacterBody.velocity.y += JUMP_VELOCITY * delta
+	elif Input.is_action_just_released("jump"):
+		JumpTimer.stop()
+		APPLIED_JUMP_ACCELERATION = 0
+		CharacterBody.velocity.y -= JUMP_VELOCITY * delta
+		JUMP_VELOCITY = JUMP_START_SPEED
+	if not JumpBuffer.is_stopped() and CharacterBody.is_on_floor():
+		JumpTimer.start(0.3)
+
+func rotate_based_on_velocity(delta):
+	var angle = Vector3.FORWARD.angle_to(VelocityVector.target_position)
+	if Vector3.LEFT.angle_to(VelocityVector.target_position) > Vector3.RIGHT.angle_to(VelocityVector.target_position):
+		angle = -angle
+	VectorPitch.rotation.y = angle
+	Graphic.rotation.y = lerp_angle(GraphicTwist.rotation.y, angle, delta * 15)
+	
+func rotation_reset(delta):
+	Graphic.rotation.x = lerp_angle(Graphic.rotation.x, 0, delta * 5)
+	VectorTwist_x.rotation.x = 0
+	Graphic.rotation.z = lerp_angle(Graphic.rotation.z, 0, delta * 5)
+	VectorTwist_z.rotation.z = 0
--- scripts/sonic_like.gd
+++ scripts/sonic_like.gd
@@ -19,26 +19,34 @@ func _ready():
 	CharacterBody.floor_max_angle = 88.9 * (PI/180)
 
 func _physics_process(delta):
+	CharacterBody.set_motion_mode(CharacterBody.MOTION_MODE_GROUNDED)
 	VelocityVector.target_position = CharacterBody.velocity / 10
-	if player_state == player_states.FLYING:
-		apply_gravity(delta)
-	elif player_state == player_states.GROUNDED:
-		get_directional_input()
-		horizontal_movement(delta)
-		if player_state == player_states.BREAKS:
-			horizontal_velocity = horizontal_velocity.move_toward(Vector3.ZERO, LOSS_BREAKS * delta)
-		CharacterBody.velocity = horizontal_velocity
-		if horizontal_velocity == Vector3.ZERO:
-			player_state = player_states.GROUNDED
-	elif player_state == player_states.BREAKS:
-		horizontal_velocity = horizontal_velocity.move_toward(Vector3.ZERO, LOSS_BREAKS * delta)
-		CharacterBody.velocity = horizontal_velocity
-		if horizontal_velocity == Vector3.ZERO:
-			player_state = player_states.GROUNDED
-			movement_reset()
 	if CharacterBody.is_on_floor():
 		if player_state != player_states.BREAKS:
 			player_state = player_states.GROUNDED
 	else:
 		player_state = player_states.FLYING
+	if player_state == player_states.FLYING:
+		apply_gravity(delta)
+		rotation_reset(delta)
+	elif player_state == player_states.GROUNDED:
+		rotate_player_body(delta, 0, false)
+		rotate_player_body(delta, 1, true)
+		if player_state == player_states.FLOATING:
+			CharacterBody.set_motion_mode(CharacterBody.MOTION_MODE_FLOATING)
+		else:
+			get_directional_input()
+			horizontal_movement(delta)
+			if player_state == player_states.BREAKS:
+				breaks(delta)
+			else:
+				CharacterBody.apply_floor_snap()
+				CharacterBody.velocity = horizontal_velocity
+		if CharacterBody.get_real_velocity() != Vector3.ZERO:
+			rotate_based_on_velocity(delta)
+	elif player_state == player_states.BREAKS:
+		rotate_player_body(delta, 0, false)
+		rotate_player_body(delta, 1, true)
+		breaks(delta)
+	jump_input(delta)
 	CharacterBody.move_and_slide()
